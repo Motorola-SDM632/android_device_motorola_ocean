59088c75c84ccffbbb3501411d4eab18800b16c8
L1ghtzin <lightzin021@gmail.com>
2024-03-02 17:36:37 -0300

    ocean: Initial AOSPA bringup

--- device.mk
+++ device.mk
@@ -1,26 +1,9 @@
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Vendor blobs
 $(call inherit-product, vendor/motorola/ocean/ocean-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     AvoidAppsInCutoutOverlay \
@@ -61,10 +44,6 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_PACKAGES += \
     init.ocean.rc
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # NFC and Secure Element packages
 PRODUCT_COPY_FILES += \
